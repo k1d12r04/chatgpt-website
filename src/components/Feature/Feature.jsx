a7413fb93a8ef6a1300b73e8785446486b034deb
Kadir <k1d12r04@gmail.com>
2023-06-10 15:26:50 +0300

    Finish whatgpt3 section and made feature component

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,7 +1,17 @@
 import './feature.css';
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = props => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div></div>
+        <h1>{props.title} </h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{props.text} </p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
