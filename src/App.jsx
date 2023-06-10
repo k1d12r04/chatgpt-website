61a7ed9a15b7178cb467e83b7139e45ced4ca020
Kadir <k1d12r04@gmail.com>
2023-06-10 19:37:59 +0300

    Add scroll to top button

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,23 @@ import {
 
 import { Cta, Brand, Navbar } from './components';
 import './App.css';
+import { FiArrowUpCircle } from 'react-icons/fi';
 
 const App = () => {
   return (
     <div className="App">
+      <svg width="0" height="0">
+        <linearGradient
+          id="purple-gradient"
+          x1="100%"
+          y1="100%"
+          x2="0%"
+          y2="0%"
+        >
+          <stop stopColor="#ae67fa" offset="0%" />
+          <stop stopColor="#f49867" offset="100%" />
+        </linearGradient>
+      </svg>
       <div className="gradient__bg">
         <Navbar />
         <Header />
@@ -24,6 +37,20 @@ const App = () => {
       <Cta />
       <Blog />
       <Footer />
+      <FiArrowUpCircle
+        className="scroll--arrow-up"
+        onClick={() => {
+          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        }}
+        size={50}
+        style={{
+          position: 'fixed',
+          bottom: '2rem',
+          right: '1rem',
+          cursor: 'pointer',
+          fill: 'url(#purple-gradient)',
+        }}
+      />
     </div>
   );
 };
