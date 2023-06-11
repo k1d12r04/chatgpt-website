f11be2a29af37308b658814c7fa500f6c97411d8
Kadir <k1d12r04@gmail.com>
2023-06-11 16:17:54 +0300

    Hide scroll up button while at the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,28 @@ import {
 import { Cta, Brand, Navbar } from './components';
 import './App.css';
 import { FiArrowUpCircle } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.scrollY > 500
+        ? setShowScrollButton(true)
+        : setShowScrollButton(false);
+    };
+    window.addEventListener('scroll', handleScrollButtonVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollButtonVisibility);
+    };
+  }, []);
+
   return (
     <div className="App">
       <svg width="0" height="0">
@@ -37,20 +57,20 @@ const App = () => {
       <Cta />
       <Blog />
       <Footer />
-      <FiArrowUpCircle
-        className="scroll--arrow-up"
-        onClick={() => {
-          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-        }}
-        size={50}
-        style={{
-          position: 'fixed',
-          bottom: '2rem',
-          right: '1rem',
-          cursor: 'pointer',
-          fill: 'url(#purple-gradient)',
-        }}
-      />
+      {showScrollButton && (
+        <FiArrowUpCircle
+          className="scroll--arrow-up"
+          onClick={handleScrollToTop}
+          size={50}
+          style={{
+            position: 'fixed',
+            bottom: '2rem',
+            right: '1rem',
+            cursor: 'pointer',
+            fill: 'url(#purple-gradient)',
+          }}
+        />
+      )}
     </div>
   );
 };
